5a785e66ca647bb9c8de3d1be54ac1864db00117
Wilhan Almeida <wilhan.amello@gmail.com>
2026-05-17 16:10:15 -0300

    controle do servo com joystick

--- src/main.cpp
+++ src/main.cpp
@@ -11,19 +11,17 @@ Servo myservo;  // create servo object to control a servo
 int posVal = 0;    // variable to store the servo position
 int servoPin = 15; // Servo motor pin
 
+int xPin = 25;
+
 void setup() {
+  pinMode(xPin, INPUT_PULLUP);
   myservo.setPeriodHertz(50);           // standard 50 hz servo
   myservo.attach(servoPin, 500, 2500);  // attaches the servo on servoPin to the servo object
 }
 void loop() {
+  int xVal = analogRead(xPin);
+  xVal = map(xVal,0, 4095, 0, 180);
 
-  for (posVal = 0; posVal <= 180; posVal += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo.write(posVal);       // tell servo to go to position in variable 'pos'
-    delay(15);                   // waits 15ms for the servo to reach the position
-  }
-  for (posVal = 180; posVal >= 0; posVal -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(posVal);       // tell servo to go to position in variable 'pos'
-    delay(15);                   // waits 15ms for the servo to reach the position
-  }
+  myservo.write(xVal);
+  delay(15);
 }
